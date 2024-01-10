3dd72a489cd28adad9308bdfc02380fbb8b282b8
unknown <ptrol123@naver.com>
2024-01-10 17:24:10 +0900

    악기 상태를 관리하는 InstrumentManager / 악기 상태 Enum

---
+++ Assets/Scripts/Manager/InstrumentManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstrumentManager : MonoBehaviour
+{
+    private Dictionary<string, InstrumentState> instrumentStates = new Dictionary<string, InstrumentState>();
+
+    private void Awake()
+    {
+        
+
+
+    }
+
+
+
+
+
+    /// <summary>
+    /// 악기 이름 / 원하는 악기 상태 입력. 
+    /// Dict에 존재하지 않으면 해당 상태를 가진채로 저장
+    /// 이미 존재하는 경우 상태를 업데이트
+    /// </summary>
+    /// <param name="instrumentName"></param>
+    /// <param name="state"></param>
+    public void SetInstrumentState(string instrumentName, InstrumentState state)
+    {
+        if(!instrumentStates.ContainsKey(instrumentName))
+        {
+            instrumentStates.Add(instrumentName, state);
+        }
+        else
+        {
+            instrumentStates[instrumentName] = state;
+        }
+    }
+
+    /// <summary>
+    /// 악기 현재 상태를 리턴
+    /// InstrumentStates에 존재하는 키인 경우 상태를 리턴
+    /// </summary>
+    /// <param name="instrumentName"></param>
+    /// <returns></returns>
+    public InstrumentState GetInstrumentState(string instrumentName)
+    {
+        if(instrumentStates.ContainsKey(instrumentName))
+        {
+            return instrumentStates[instrumentName];
+        }
+        else
+        {
+            return InstrumentState.Play;
+        }
+    }
+}
--- Assets/Scripts/Manager/StageManager.cs
+++ Assets/Scripts/Manager/StageManager.cs
@@ -8,7 +8,7 @@ public class StageManager : MonoBehaviour
     /// 스테이지 선택창에서의 스테이지 정보를 보여주는 기능
     /// 최고점수, 클리어 상황 등등
     /// </summary>
-    private Dictionary<string, int> highestScores = new Dictionary<string, int>;
+    private Dictionary<string, int> highestScores = new Dictionary<string, int>();
 
     void Start()
     {
