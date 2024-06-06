f1d199240c5042c8c095d3ec6a939ed82e5c4f59
junho1144 <jungpun1324@gmail.com>
2024-06-06 11:11:37 +0900

    클래스명 및 변수명 변경
    클래스명을 인스펙터의 스크립트와 동일하게 변경,
    변수명이 그 변수의 기능을 대변하도록 변경

--- Assets/Scripts/SaturationChanger.cs
+++ Assets/Scripts/SaturationChanger.cs
@@ -2,24 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HSVchanger : MonoBehaviour
+public class Saturationchanger : MonoBehaviour
 {
     private Renderer _renderer;
     private MaterialPropertyBlock _propBlock;
 
-    private float value;
+    private float splitValue;
 
     void Start()
     {
         _renderer = GetComponent<Renderer>();
         _propBlock = new MaterialPropertyBlock();
 
-        value  = 0f;
+        splitValue  = 0f;
     }
 
-    public void SaturationChange(float v){
-        value = v;
-        _propBlock.SetFloat("_Split_Value", value);
+    public void ChangeSaturation(float proportion){
+        splitValue = proportion;
+        _propBlock.SetFloat("_Split_Value", splitValue);
         _renderer.SetPropertyBlock(_propBlock);
     }
 }
